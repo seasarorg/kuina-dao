db533a57476160003968e48f5ed5f3c0ef02be72
nakamura-to <toshihiro.nakamura@gmail.com>
2006-12-12 14:34:15 +0000

    OneToManyInverse を使った Named Query のテストケースを追加しました。
    git-svn-id: https://www.seasar.org/svn/kuina/trunk@254 8b831ddc-e003-0410-88a7-db5ebb3dcf8c

---
+++ kuina-dao-it/src/test/java/org/seasar/kuina/dao/it/named/dao/AbstractOneToManyInverseDaoTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.kuina.dao.it.named.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.seasar.kuina.dao.it.entity.OneToManyInverse;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * 
+ * @author nakamura
+ */
+public class AbstractOneToManyInverseDaoTest {
+
+    private EntityManager em;
+
+    private OneToManyInverseDao dao;
+
+    public void updateNameById() throws Exception {
+        dao.updateNameById(1, "Marketing");
+        OneToManyInverse inverse = em.find(OneToManyInverse.class, 1);
+        assertEquals("Marketing", inverse.getName());
+    }
+
+    public void findByName() throws Exception {
+        List<OneToManyInverse> list = dao.findByName("%c%");
+        assertNotNull(list);
+        assertEquals(2, list.size());
+        assertEquals("Account", list.get(0).getName());
+        assertEquals("Purchase", list.get(1).getName());
+    }
+
+}
