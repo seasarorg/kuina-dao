aa049d6381d4bdcc1b37294765c61314343a6d65
Koichi Kobayashi <koichik@improvement.jp>
2007-04-12 07:08:27 +0000

    Javadoc 微修正．
    git-svn-id: https://www.seasar.org/svn/kuina/trunk@419 8b831ddc-e003-0410-88a7-db5ebb3dcf8c

--- kuina-dao/src/main/java/org/seasar/kuina/dao/criteria/grammar/PathExpression.java
+++ kuina-dao/src/main/java/org/seasar/kuina/dao/criteria/grammar/PathExpression.java
@@ -26,10 +26,13 @@ import org.seasar.kuina.dao.criteria.Criterion;
  * <pre>
  * single_valued_path_expression ::=
  *     state_field_path_expression | single_valued_association_path_expression
+ * 
  * state_field_path_expression ::=
  *     {identification_variable | single_valued_association_path_expression}.state_field
+ * 
  * single_valued_association_path_expression ::=
- *     identification_variable.{single_valued_association_field.}* single_valued_association_field
+ *     identification_variable.{single_valued_association_field.}*single_valued_association_field
+ * 
  * collection_valued_path_expression ::=
  *     identification_variable.{single_valued_association_field.}*collection_valued_association_field
  * </pre>
