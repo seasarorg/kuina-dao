09252aa2cb90d427bc977cc2c5a127df8cb83ba5
Koichi Kobayashi <koichik@improvement.jp>
2009-04-18 11:33:52 +0000

    [KUINA-18] エンティティクラス名が JPQL の予約語とマッチする場合，自動生成される JPQL の identification_variable が予約語のままになってしまう問題を修正しました．[Seasar-user:17269] https://www.seasar.org/issues/browse/KUINA-18 http://ml.seasar.org/archives/seasar-user/2009-April/017270.html
    git-svn-id: https://www.seasar.org/svn/kuina/trunk@580 8b831ddc-e003-0410-88a7-db5ebb3dcf8c

--- kuina-dao/src/main/java/org/seasar/kuina/dao/internal/util/JpqlUtil.java
+++ kuina-dao/src/main/java/org/seasar/kuina/dao/internal/util/JpqlUtil.java
@@ -15,6 +15,10 @@
  */
 package org.seasar.kuina.dao.internal.util;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.seasar.framework.jpa.metadata.EntityDesc;
 import org.seasar.framework.util.StringUtil;
 
@@ -24,6 +28,84 @@ import org.seasar.framework.util.StringUtil;
  * @author koichik
  */
 public class JpqlUtil {
+
+    private static final Set<String> reservedIdentifiers;
+    static {
+        Set<String> set = new HashSet<String>(55);
+        set.add("SELECT");
+        set.add("FROM");
+        set.add("WHERE");
+        set.add("UPDATE");
+        set.add("DELETE");
+        set.add("JOIN");
+        set.add("OUTER");
+        set.add("INNER");
+        set.add("LEFT");
+        set.add("GROUP");
+        set.add("BY");
+        set.add("HAVING");
+        set.add("FETCH");
+        set.add("DISTINCT");
+        set.add("OBJECT");
+        set.add("NULL");
+        set.add("TRUE");
+        set.add("FALSE");
+        set.add("NOT");
+        set.add("AND");
+        set.add("OR");
+        set.add("BETWEEN");
+        set.add("LIKE");
+        set.add("IN");
+        set.add("AS");
+        set.add("UNKNOWN");
+        set.add("EMPTY");
+        set.add("MEMBER");
+        set.add("OF");
+        set.add("IS");
+        set.add("AVG");
+        set.add("MAX");
+        set.add("MIN");
+        set.add("SUM");
+        set.add("COUNT");
+        set.add("ORDER");
+        set.add("BY");
+        set.add("ASC");
+        set.add("DESC");
+        set.add("MOD");
+        set.add("UPPER");
+        set.add("LOWER");
+        set.add("TRIM");
+        set.add("POSITION");
+        set.add("CHARACTER_LENGTH");
+        set.add("CHAR_LENGTH");
+        set.add("BIT_LENGTH");
+        set.add("CURRENT_TIME");
+        set.add("CURRENT_DATE");
+        set.add("CURRENT_TIMESTAMP");
+        set.add("NEW");
+        set.add("EXISTS");
+        set.add("ALL");
+        set.add("ANY");
+        set.add("SOME");
+        reservedIdentifiers = Collections.unmodifiableSet(set);
+    }
+
+    private static IdentificationVariableStrategy strategy = new DefaultIdentificationVariableStrategy();
+
+    /**
+     * {@link IdentificationVariableStrategy}を設定します．
+     * 
+     * @param strategy
+     *            strategy
+     * @throws NullPointerException
+     *             {@code strategy}が{@literal null}の場合
+     */
+    public static void setStrategy(IdentificationVariableStrategy strategy) {
+        if (strategy == null) {
+            throw new NullPointerException("strategy");
+        }
+        JpqlUtil.strategy = strategy;
+    }
 
     /**
      * エンティティクラスをabstract_schema_nameに変換して返します．
@@ -60,8 +142,58 @@ public class JpqlUtil {
      */
     public static String toDefaultIdentificationVariable(
             final String abstractSchemaName) {
-        return StringUtil.decapitalize(abstractSchemaName
-                .substring(abstractSchemaName.lastIndexOf('.') + 1));
+        return strategy.toIdentificationVariable(abstractSchemaName);
+    }
+
+    /**
+     * abstract_schema_nameからidentification_variableを求める方法を提供します．
+     * 
+     * @author koichik
+     */
+    public interface IdentificationVariableStrategy {
+
+        /**
+         * abstract_schema_nameからidentification_variableを求めて返します．
+         * 
+         * @param abstractSchemaName
+         *            abstract_schema_name
+         * @return identification_variable
+         */
+        String toIdentificationVariable(String abstractSchemaName);
+    }
+
+    /**
+     * デフォルトの{@link IdentificationVariableStrategy}です．
+     * 
+     * @author koichik
+     */
+    public static class DefaultIdentificationVariableStrategy implements
+            IdentificationVariableStrategy {
+
+        public String toIdentificationVariable(final String abstractSchemaName) {
+            final boolean reserved = reservedIdentifiers
+                    .contains(abstractSchemaName.toUpperCase());
+            return StringUtil.decapitalize(abstractSchemaName
+                    .substring(abstractSchemaName.lastIndexOf('.') + 1))
+                    + (reserved ? "_" : "");
+        }
+
+    }
+
+    /**
+     * Kuina-Dao 1.0.2以前と同じidentification_variableを返す
+     * {@link IdentificationVariableStrategy}です．
+     * 
+     * @author koichik
+     */
+    public static class CompatibleIdentificationVariableStrategy implements
+            IdentificationVariableStrategy {
+
+        public String toIdentificationVariable(final String abstractSchemaName) {
+            return StringUtil.decapitalize(abstractSchemaName
+                    .substring(abstractSchemaName.lastIndexOf('.') + 1));
+        }
+
     }
 
 }
